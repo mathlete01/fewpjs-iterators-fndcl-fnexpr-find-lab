4323b0f5ca8e407ab19a1e6f3ee8510a99069d87
mathlete01 <msallin@gmail.com>
2020-11-30 08:56:06 -0800

    commit tree 869bd1deaf6716c0a45ee27f118ef8ed0f72487c

--- index.js
+++ index.js
@@ -3,3 +3,32 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(array){
+  //return array.result === "W"
+  array.find( function(s){
+    if(s.result === "W"){
+      return s
+    }
+  })
+}
+
+const inventory = [
+  {name: 'apples', quantity: 2},
+  {name: 'bananas', quantity: 0},
+  {name: 'cherries', quantity: 5}
+];
+
+function findCherries(arr){
+  arr.find(function(fruit){
+    return fruit.name === "cherries"
+  })
+}
+
+findCherries(inventory)
